005f3e30d273ea039a465cde6722e79451e3ce83
Mateusz Bochynski (PGS Software) <mbochynski@pgs-soft.com>
2018-06-25 08:32:59 +0200

    beforeall/each, disable tests,

--- spec/your_first_suite.test.js
+++ spec/your_first_suite.test.js
@@ -28,10 +28,75 @@ describe("A spec using the fail function", function() {
   };
 
   it("should not call the callBack", function() {
-    foo(true, function() {
+    foo(false, function() {
       fail("Callback has been called");
     });
   });
 });
 
+// Before a spec is executed,
+// Jasmine walks down the tree executing each 
+// beforeEach function in order. 
+// After the spec is executed,
+// Jasmine walks through the afterEach functions similarly.
+describe("A spec", function() {
+  var foo;
 
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  afterEach(function() {
+    foo = 0;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+
+  describe("nested inside a second describe", function() {
+    var bar;
+
+    beforeEach(function() {
+      bar = 1;
+    });
+
+    it("can reference both scopes as needed", function() {
+      expect(foo).toEqual(bar);
+    });
+  });
+});
+
+// disable suite
+xdescribe("A spec", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+});
+
+// disable tests
+describe('Pending specs', function() {
+  xit("can be declared 'xit'", function() {
+    expect(true).toBe(false);
+  });
+
+  it("can be declared with 'it' but without a function");
+
+  it("can be declared by calling 'pending' in the spec body", function() {
+    expect(true).toBe(false);
+    pending('this is why it is pending');
+  });
+});
